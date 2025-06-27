8df3e1ad99e55322cc35b6f2906825da28bb1f10
KimSiJin <kimsijin33@gmail.com>
2025-06-27 11:16:00 +0900

    feat: Print fizzbuzz If i is Divisible by 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,9 @@
 print('hello')
 
 for i in range(1,16+1): #3의 배수
-    if i%3==0:
+    if i % 15 == 0:
+        print('fizzbuzz') #공배수 처리
+    elif i%3==0:
         print('fizz')
     elif i % 5 == 0:
         print('buzz')
